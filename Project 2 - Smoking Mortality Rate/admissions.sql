1dfd5523fd27afc639f3fa059fa0947aba6bdb24
AshwinAshok3 <149888800+AshwinAshok3@users.noreply.github.com>
2025-03-07 16:26:22 +0530

    Add files via upload

---
+++ Project 2 - Smoking Mortality Rate/admissions.sql
@@ -0,0 +1,5 @@
+use smoking_mortality;
+select * from admissions;
+select * from metrics;
+select * from prescriptions;
+select * from smokers;
---
+++ Project 2 - Smoking Mortality Rate/smoking_mortality_.sql
@@ -0,0 +1,6 @@
+use smoking_mortality;
+select * from admissions;
+select * from fatalities;
+select * from metrics;
+select * from prescriptions;
+select * from smokers;
